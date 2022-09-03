8f1db10c93753b244314e19d8aad1cb37c3367be
Andrey <boyko8120@gmail.com>
2022-09-03 11:49:47 +0300

    add OrderedCollections lib set instead of array

--- ViewControlers/HomeViewController.swift
+++ ViewControlers/HomeViewController.swift
@@ -1,11 +1,12 @@
 
 import UIKit
+import OrderedCollections
 
 class HomeViewController: UIViewController {
     
     var homeView = MainView()
     let reuseIdentifier = "cell"
-    var movies = [Movie(title: "Interstellar", year: 2014), Movie(title: "The Shawshank Redemption", year: 1994)]
+    var moviesSet:  OrderedSet<Movie> = [Movie(title: "Interstellar", year: 2014), Movie(title: "The Shawshank Redemption", year: 1994)]
     
     //MARK: - LifeCycle
     override func loadView() {
@@ -20,45 +21,39 @@ class HomeViewController: UIViewController {
         homeView.addButton.addTarget(self, action: #selector(tapButton), for: .touchDown)
     }
     
-    override func viewDidLayoutSubviews() {
-       
-    }
-    
     //MARK: - Action
     
     @objc func tapButton(sender: UIButton!) {
         let modelMovie = Movie(
-            title: homeView.textFieldTitle.text ?? "",
-            year: Int(homeView.textFeildYear.text ?? "0") ?? 0)
-        let year = Int(homeView.textFeildYear.text ?? "0") ?? 0
-        var duplicate = false
-        movies.forEach({
-            if $0.title.trimmingCharacters(in: .whitespaces) == homeView.textFieldTitle.text?.trimmingCharacters(in: .whitespaces) && $0.year == year {
-            duplicate = true
-            }
-        })
-        if homeView.textFieldTitle.hasText && (year > 1900 && year < 2030) && duplicate == false {
-            movies.insert(modelMovie, at: 0)
-            let indexPath = IndexPath(row: 0, section: 0)
-            homeView.moviesTableView.insertRows(at: [indexPath], with: .bottom)
-            duplicate = false
+            title: homeView.textFieldTitle.text?.trimmingCharacters(in: .whitespaces) ?? "",
+            year: Int(homeView.textFeildYear.text?.trimmingCharacters(in: .whitespaces) ?? "0") ?? 0)
+        let movieContaints = moviesSet.contains(modelMovie)
+        if homeView.textFieldTitle.hasText && (modelMovie.year > 1900 && modelMovie.year < 2030) && movieContaints == false {
+            addToFavourite(modelMovie: modelMovie)
         }
     }
+    
+    func addToFavourite(modelMovie: Movie) {
+        moviesSet.insert(modelMovie, at: 0)
+        let indexPath = IndexPath(row: 0, section: 0)
+        homeView.moviesTableView.insertRows(at: [indexPath], with: .bottom)
+    }
 }
-//MARK: - TableViewDelegate
 
-extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    //MARK: - TableViewDelegate
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return movies.count
+    extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return moviesSet.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            tableView.register(MoviesTableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+            let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier) as? MoviesTableViewCell
+            cell?.setupCell(moive: moviesSet[indexPath.row])
+            return cell ?? UITableViewCell()
+        }
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        tableView.register(MoviesTableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
-        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier) as? MoviesTableViewCell
-        cell?.setupCell(moive: movies[indexPath.row])
-        return cell ?? UITableViewCell()
-    }
-}
 
 
